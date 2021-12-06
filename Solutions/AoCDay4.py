ef84de40e1675427ad2db5641a86dc3c5f7593c9
Colin Wisdom <colin.wisdom@wholefoods.com>
2021-12-06 16:46:15 -0600

    Completed day 4

--- Solutions/AoCDay4.py
+++ Solutions/AoCDay4.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-input = open('../PuzzleData/day4_puzzle_input.txt').read()
 
-draw, boards = input.split('\n', 1)
-draw = [int(i) for i in draw.split(',')]
-boards = boards.strip().split('\n\n')
+with open('../PuzzleData/day4_puzzle_input.txt') as my_file:
+    data = my_file.read()
 
-boards = [
-    np.array([[int(j) for j in i.split(' ') if j != ''] for i in board.strip().split('\n')])
-    for board in boards
-]
+    draw, boards = data.split('\n', 1)
+    draw = [int(i) for i in draw.split(',')]
+    boards = boards.strip().split('\n\n')
 
-def checkIfWon(board):
+    boards = [
+        np.array([[int(j) for j in i.split(' ') if j != ''] for i in board.strip().split('\n')])
+        for board in boards
+    ]
+
+def check_win(board):
     for y in range(board.shape[0]):
         if np.all(board[y,:] < 0): return True
 
@@ -20,7 +22,7 @@ def checkIfWon(board):
 
     return False
 
-def result(board, call):
+def score(board, call):
     return np.sum(board[board > 0]) * call
 
 winner = []
@@ -28,9 +30,9 @@ for call in draw:
     for i in range(len(boards)-1,-1,-1):
         board = boards[i]
         board[board == call] *= -1
-        if checkIfWon(board):
+        if check_win(board):
             winner.append((board, call))
             boards.pop(i)
 
-print('Part 1:' , result(*winner[0]))
-print('Part 2:' , result(*winner[len(winner)-2]))
+print('Part 1:', score(*winner[0]))
+print('Part 2:', score(*winner[len(winner) - 2]))
